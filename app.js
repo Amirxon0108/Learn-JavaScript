f50e82970741e94195e16e7ca9fa068da816fa3f
amirdevvf <amirdevfullstack@gmail.com>
2026-02-02 14:19:05 +0300

    for while do while and if

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-let email = 'amirxonmatchanov@gmail.com'
+let email = 'amirx  `nmatchanov@gmail.com'
 console.log(email.indexOf('o')) 
- 
+
+ let name = prompt('ismingizini kiriting!')
+let boshHarfi = name.charAt().toUpperCase()
+let davomi  = name.slice(1).toLowerCase()
+let result = boshHarfi + davomi
+alert(`Salom, ${result}`)
+
+
+let i = 0 
+for (i; i< 10; i++){
+    console.log('amir')
+}
+
+const names = ['ahror', 'sanjar', 'ali']
+
+for ( let i = 0; i< names.length; i++){
+    let boshHarfi = names[i].charAt().toUpperCase()
+let davomi  = names[i].slice(1).toLowerCase()
+let result = boshHarfi + davomi
+    console.log(`${result}`)
+}
